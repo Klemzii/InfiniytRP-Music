b6a59985da25c1c5e32a48ee53fe0c65561ddc49
Klemzii <163579368+Klemzii@users.noreply.github.com>
2024-06-25 14:46:09 +0200

    Update help.js

--- basicCommands/help.js
+++ basicCommands/help.js
@@ -13,11 +13,11 @@ module.exports = {
       .setColor('#2b71ec')
       .setAuthor({
         name: 'Im here to Help!',
-        iconURL: 'https://cdn.discordapp.com/attachments/1175487983915376662/1175667506791325706/communication.png?ex=656c10b0&is=65599bb0&hm=e378f1b355a2401bcab504b08a0766001d6b7c090c91ce0a7a7a87c868feb955&', 
-        url: 'https://discord.gg/FUEHs7RCqz'
+        iconURL: 'https://cdn.discordapp.com/attachments/1136698225684922389/1255141289872199740/server_logo.png?ex=667c0cea&is=667abb6a&hm=c01e669cb19b8d757665c5e4f60a8f2cf674966c54761270873dbe3f81e43f2b&', 
+        url: 'https://discord.gg/PpvGGdDskT'
     })
      
-      .setDescription(`__**STATS :**__\n\n> **📊 Bot in servers:** ${serverCount}\n> **🟢 Bot Ping:** ${botPing}ms\n> **👑 Made By [GlaceYT](https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A)**\n\n__**COMMANDS :**__ `)
+      .setDescription(`__**STATS :**__\n\n> **📊 Bot in servers:** ${serverCount}\n> **🟢 Bot Ping:** ${botPing}ms\n> **👑 Made For [InfiniytRP](https://discord.gg/PpvGGdDskT)**\n\n__**COMMANDS :**__ `)
       .addFields(
       
         {
@@ -43,13 +43,7 @@ module.exports = {
           value: '`cat`, `dog`',
           inline: true,
         },
-      
-        {
-          name: '▶️  Anime',
-          value: '`<prefix>animecommands for more info`',
-          inline: true,
-        },
-     
+
         {
           name: '▶️  Utility',
           value: '`kick`, `ban`, `serverinfo`,`userinfo`, `clear`',
@@ -57,21 +51,21 @@ module.exports = {
         }
       )
       .setThumbnail(botUser.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
-      .setImage(`https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=6641e8b7&is=66409737&hm=96f69ba8bcde4ca44bf8713e22001e0bafad0335cfd1f57c439a34b4762f0e03&`);
+      .setImage(`https://cdn.discordapp.com/attachments/1136698225684922389/1255141289872199740/server_logo.png?ex=667c0cea&is=667abb6a&hm=c01e669cb19b8d757665c5e4f60a8f2cf674966c54761270873dbe3f81e43f2b&`);
 
     const button1 = new ButtonBuilder()
-      .setLabel('YouTube')
-      .setURL('https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A')
+      .setLabel('Web')
+      .setURL('https://infinityrp.si/')
       .setStyle(ButtonStyle.Link);
 
     const button2 = new ButtonBuilder()
       .setLabel('Discord')
-      .setURL('https://discord.gg/xQF9f9yUEM')
+      .setURL('https://discord.gg/PpvGGdDskT')
       .setStyle(ButtonStyle.Link);
 
     const button3 = new ButtonBuilder()
-      .setLabel('Code')
-      .setURL('https://github.com/GlaceYT')
+      .setLabel('Server')
+      .setURL('http://cfx.re/join/v7v9ve')
       .setStyle(ButtonStyle.Link);
       
     const row = new ActionRowBuilder()
