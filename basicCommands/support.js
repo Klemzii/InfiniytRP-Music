541d3e57be6335af6eb6d7c47c55ac8f237fe903
Klemzii <163579368+Klemzii@users.noreply.github.com>
2024-06-25 15:08:06 +0200

    Update support.js

--- basicCommands/support.js
+++ basicCommands/support.js
@@ -4,19 +4,19 @@ module.exports = {
     name: 'support',
     description: 'support server of this Bot',
     execute(message, args) {
-        const supportServerLink = "https://discord.gg/xQF9f9yUEM";
-      const githubLink = "https://github.com/GlaceYT";
-      const replitLink = "https://replit.com/@GlaceYT";
-      const youtubeLink = "https://www.youtube.com/@GlaceYT";
+        const supportServerLink = "https://discord.gg/PpvGGdDskT";
+      const githubLink = "https://infinityrp.si/";
+      const replitLink = "https://infinityrp.si/";
+      const youtubeLink = "http://cfx.re/join/v7v9ve";
         const embed = new EmbedBuilder()
             .setColor('#b300ff')
             .setAuthor({
               name: 'Support Server',
-              iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
+              iconURL: 'https://cdn.discordapp.com/attachments/1200423304176078881/1255146421988888596/Infinity_RP.png?ex=667c11b2&is=667ac032&hm=6d86ab9e1089522013432c64f95b1be8733872bcbbaf83e98ae903897105a49d&', 
               url: 'https://discord.gg/xQF9f9yUEM'
           })
-            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
-            .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&')
+            .setDescription(`➡️ **Pridruži se našemu discordu!:**\n- Discord - ${supportServerLink}\n\n➡️ **Ostala socialna omrežja:**\n- Web - ${githubLink}\n- TikTok - ${replitLink}\n- Strežnik - ${youtubeLink}`)
+            .setImage('https://cdn.discordapp.com/attachments/1200423304176078881/1255146421988888596/Infinity_RP.png?ex=667c11b2&is=667ac032&hm=6d86ab9e1089522013432c64f95b1be8733872bcbbaf83e98ae903897105a49d&')
             .setTimestamp();
 
         message.reply({ embeds: [embed] });
